ec49dd6fd1ff1203182a379a5b349223cdb77f7f
gaurav <payme4work17461@gmail.com>
2024-05-06 17:40:25 +0530

    created Navbar

---
+++ client/src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import {Routes,Route,BrowserRouter} from "react-router-dom"
+import Home from './components/Home'
+import Navbar from './components/Navbar'
+
+const App = () => {
+  return (
+    <>
+    <BrowserRouter>
+    <Navbar/>
+    <Routes>
+      <Route path="/" element={<Home/>}/>
+    </Routes>
+    </BrowserRouter>
+    </>
+  )
+}
+
+export default App
---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,38 @@
+import { FaShoppingCart, FaBars, FaSearch } from "react-icons/fa";
+
+const Navbar = () => {
+  return (
+    <div className="flex justify-between items-center bg-white shadow-lg h-20 px-8">
+      
+      <div className="flex space-x-40">
+        <FaBars className="h-8 w-8 hover:text-teal-600 sm:hidden" />
+        <h1 className="font-bold text-3xl ">foody</h1>
+        <div className="sm:hidden ml-4">
+          <FaSearch className="h-8 w-8 text-black hover:text-teal-600" />
+        </div>
+      </div>
+
+      <div className="hidden lg:flex items-center rounded-lg border border-black p-2">
+        <FaSearch className="text-black" />
+        <input
+          className="p-2 ml-2 text-black placeholder-white focus:outline-none"
+          placeholder="Search..."
+        />
+      </div>
+
+      <div className="flex items-center space-x-8">
+        <button className="hover:bg-slate-200 text-black sm:block hidden font-semibold px-4 py-2 rounded-sm shadow-md text-xl border border-black flex-shrink-0">
+          Sign up
+        </button>
+        <button className="hover:bg-slate-200 text-black sm:block hidden font-semibold px-4 py-2 rounded-sm shadow-md text-xl border border-black flex-shrink-0">
+          Log in
+        </button>
+        <button className="sm:block mb-2">
+          <FaShoppingCart className="h-8 w-8 hover:text-teal-600" />
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
